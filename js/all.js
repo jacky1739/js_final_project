a3790c35a79dee5604cfedd2d4bdd80d0132bc88
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-04-20 00:37:47 +0800

    product list

--- js/all.js
+++ js/all.js
@@ -1,3 +1,45 @@
-const api_path = "jacky";
-const token = "ivXuchXybveASGVr7GenJDvWSYW2"
+let productData = [];
+
+init();
+
+function init(){
+    getProductList();
+}
+
+function getProductList(){
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${api_path}/products`)
+        .then(function (response) {
+        console.log(response.data.products);
+        productData = response.data.products;
+        renderProducts();
+    })
+        .catch(function (error) {
+        console.log(error);
+    })
+}
+
+function renderProducts() {
+    let str = "";
+    productData.forEach(function(item){
+        str += `
+            <li class="productCard">
+                <h4 class="productType">新品</h4>
+                <img src="${item.images}" alt="">
+                <a href="#" id="addCardBtn">加入購物車</a>
+                <h3>${item.title}</h3>
+                <del class="originPrice">NT${item.origin_price}</del>
+                <p class="nowPrice">NT${item.price}</p>
+            </li>`
+    })
+    productList.innerHTML = str;
+}
+
+/* <li class="productCard">
+    <h4 class="productType">新品</h4>
+    <img src="${item.images}" alt="">
+    <a href="#" id="addCardBtn">加入購物車</a>
+    <h3>${item.title}</h3>
+    <del class="originPrice">NT${item.origin_price}</del>
+    <p class="nowPrice">NT${item.price}</p>
+</li> */
 
---
+++ js/config.js
@@ -0,0 +1,4 @@
+const api_path = "jacky";
+const token = "ivXuchXybveASGVr7GenJDvWSYW2"
+
+const productList = document.querySelector(".productWrap");
