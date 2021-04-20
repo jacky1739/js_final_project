ca033ae53e832da9ea6fb4afeb7861b6006ea25d
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-04-20 22:29:25 +0800

    cart ok

--- js/all.js
+++ js/all.js
@@ -1,9 +1,11 @@
 let productData = [];
+let cartData  = [];
 
 init();
 
 function init(){
     getProductList();
+    getCartList();
 }
 
 function getProductList(){
@@ -25,7 +27,7 @@ function renderProducts() {
             <li class="productCard">
                 <h4 class="productType">新品</h4>
                 <img src="${item.images}" alt="">
-                <a href="#" id="addCardBtn">加入購物車</a>
+                <a href="#" id="addCardBtn" class="js-addCart" data-id="${item.id}">加入購物車</a>
                 <h3>${item.title}</h3>
                 <del class="originPrice">NT${item.origin_price}</del>
                 <p class="nowPrice">NT${item.price}</p>
@@ -33,6 +35,41 @@ function renderProducts() {
     })
     productList.innerHTML = str;
 }
+
+
+function getCartList(){
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${api_path}/carts`)
+        .then(function (response) {
+        console.log(response.data.carts)
+        cartData = response.data.carts;
+        let str = "";
+        cartData.forEach(function(item){
+            str += `
+            <tr>
+                <td>
+                    <div class="cardItem-title">
+                        <img src="${item.product.images}" alt="">
+                        <p>${item.product.title}</p>
+                    </div>
+                </td>
+                <td>NT$${item.product.price}</td>
+                <td>${item.quantity}</td>
+                <td>NT$${item.product.price * item.quantity}</td>
+                <td class="discardBtn">
+                    <a href="#" class="material-icons">
+                        clear
+                    </a>
+                </td>
+            </tr>
+            `
+        });
+        cartList.innerHTML = str;
+    })
+        .catch(function (error) {
+        console.log(error);
+    })
+}
+
 
 productSelect.addEventListener("change", function(e){
     console.log(e.target.value);
@@ -43,15 +80,52 @@ productSelect.addEventListener("change", function(e){
     productData.forEach(function(item){
         if(item.category == e.target.value){
             str += `
-            <li class="productCard">
-                <h4 class="productType">新品</h4>
-                <img src="${item.images}" alt="">
-                <a href="#" id="addCardBtn">加入購物車</a>
-                <h3>${item.title}</h3>
-                <del class="originPrice">NT${item.origin_price}</del>
-                <p class="nowPrice">NT${item.price}</p>
-            </li>`
+                <li class="productCard">
+                    <h4 class="productType">新品</h4>
+                    <img src="${item.images}" alt="">
+                    <a href="#" id="addCardBtn" class="js-addCart" data-id="${item.id}">加入購物車</a>
+                    <h3>${item.title}</h3>
+                    <del class="originPrice">NT${item.origin_price}</del>
+                    <p class="nowPrice">NT${item.price}</p>
+                </li>`
         }
     })
     productList.innerHTML = str;
 })
+
+
+productList.addEventListener("click", function(e){
+    console.log(e.target.nodeName);
+    e.preventDefault(); // 不讓一直跳到最上面
+    if(e.target.nodeName !== "A"){
+        e.preventDefault();
+        // alert("您沒點擊到加入購物車")
+        return
+    }
+    let productId = e.target.getAttribute("data-id");
+    let numCheck = 1;
+    // 當物品重複的時候，先去比較有沒有重複點擊的品項，有的話 + 1，沒有的話直接去發送
+    cartData.forEach(function(item){
+        if(item.product.id === productId){
+            numCheck = item.quantity += 1;
+        }
+    })
+
+    axios.post(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${api_path}/carts`,
+    {
+        "data": {
+            "productId": productId,
+            "quantity": numCheck
+        }
+    }).then(function (response) {
+        console.log(response);
+        alert("加入購物車成功")
+        getCartList();
+
+    })
+        .catch(function (error) {
+        console.log(error);
+    })
+
+})
+
--- js/config.js
+++ js/config.js
@@ -3,3 +3,4 @@ const token = "ivXuchXybveASGVr7GenJDvWSYW2"
 
 const productList = document.querySelector(".productWrap");
 const productSelect = document.querySelector(".productSelect");
+const cartList = document.querySelector(".shoppingCart-tableBody");
